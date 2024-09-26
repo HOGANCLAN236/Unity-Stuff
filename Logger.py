79de403e467edf2ec70b1ffdb6277440a8857cd4
HOGAN <93939923+HOGANCLAN236@users.noreply.github.com>
2024-09-26 19:27:14 +0100

    PYTHON CODE!

---
+++ Logger.py
@@ -0,0 +1,56 @@
+from discord.ext import commands
+import discord
+
+BOT_TOKEN = ""
+ReportChat = 0
+generalChat = 0
+
+SEVER_ID = 0
+
+list = ["fuck", "shit", "bitch", "nigger", "nigga", "cunt", "twat", "dick", "cock", "whore", "pussy"]
+
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print('Logger Connected!')
+    channel = bot.get_channel(generalChat)
+    await channel.send("Logger Connected!!!")
+
+@bot.event
+async def on_message(message):
+    guild = bot.get_guild(SEVER_ID)
+    
+
+    if guild is None:
+        print("No Guild")
+        return
+        
+    channel = guild.get_channel(ReportChat)
+
+    if message.author == bot.user:
+        return
+
+    for i in list:   
+        if message.content.startswith(i):
+            await channel.send("Message: " + i + " | " + "User: " + message.author.name)
+
+@bot.event
+async def on_message_edit(before, after):
+    guild = bot.get_guild(SEVER_ID)
+    
+
+    if guild is None:
+        print("No Guild")
+        return
+        
+    channel = guild.get_channel(ReportChat)
+    if after.author == bot.user:
+        return
+
+    for i in list:   
+        if after.content.startswith(i):
+            await channel.send("Edited Message: " + i + " | " + "User: " + after.author.name)
+
+
+bot.run(BOT_TOKEN)
